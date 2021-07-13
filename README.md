11f36faab43bb844268496dacc070ffbf599e109
Shivaraj <shivaraja.hm@spurtreetech.in>
2021-07-13 14:52:11 +0530

    #2 logger added

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Sop_Mint
 # NodeJS Backend application
-# Using libraries are:
-# Express, Mongoose,jsonwebtoken, express-validator,bcryptjs, cors, body-parser, multer,
+# Using libraries are: Express, Mongoose,jsonwebtoken, express-validator,bcryptjs, cors, body-parser, multer, rate-limiter, logger
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const rateLimit = require("express-rate-limit");
+var winston = require('winston'),
+    expressWinston = require('express-winston');
 const dbConfig = require('./config/configurl');
 const route = require('./routes');
 
@@ -21,6 +23,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, 
+  msg: "HTTP {{req.method}} {{req.url}} {{req.headers}}", 
+  expressFormat: true, 
+  colorize: false,
+  ignoreRoute: function (req, res) { return false; } 
+}));
 app.use('/', route);
 
 
